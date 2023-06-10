41c4bfb597912dee942038d9c9844a9872512213
shaghaygh <shqayqm145@gmail.com>
2023-06-10 15:35:11 +0330

    10/6/2023 , 15:17

--- Dev 1.cpp
+++ Dev 1.cpp
@@ -3,7 +3,92 @@
 using namespace std;
 int main()
 {
-cout<< "In The Name Of God ";
-getch();
-return 0;
+//cout<< "In The Name Of God ";
+//getch();
+//return 0;
+
+
+//int x,y;
+//cin>>x;
+//y=x*x;
+//cout<<y;
+
+
+//int a, b;
+//cin>> a >> b;
+//if (a>b)
+//cout<< a+b << endl << a-b ;
+//if (a<b)
+//cout<< a+b << "\n" << b-a ;
+
+
+//int x, y, n;
+//cin >> n;
+//x = n / 10;
+//y = n % 10;
+//cout << x+y;
+
+
+//int x, y, n;
+//cin >> n;
+//x = n / 10;
+//y = n % 10;
+//cout << y << x;
+
+
+//int r;
+//float s,p;
+//cout<<"enter radius:\n";
+//cin >> r;
+//s = 3.14 * r * r;
+//p=2 * 3.14 * r;
+//cout<< "S = " << s << endl << "P = " << p << endl;
+
+
+//char ch;
+//cout << "enter a character:";
+//ch = cin.get();
+//cout << "you typed character:" << ch;
+
+
+//int a,b,c;
+//float avg;
+//cin >> a >> b >> c ;
+//avg = float(a+b+c)/3;
+//cout<<avg;
+
+
+int n,i,j ;
+cin>>n;
+for (i=0; i<n; i++)
+cout<<"*";
+cout<<"\n";
+for (i=0 ; i<n-2; i++)
+{
+cout<<"*";
+for (j=0 ; j<n-2; j++)
+cout<<" " ;
+cout<<"*\n" ;
+}
+for (i=0; i<n; i++)
+cout<<"*";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
